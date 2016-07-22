bf0f099eabaeba4c3b3c7fd8fa71c2b735086983
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-07-22 10:40:07 -0400

    API key works. I can start appending to the page now.

--- fe/app.js
+++ fe/app.js
@@ -1,13 +1,41 @@
 window.onload = function() {
   console.log('js loaded');
 
+//get elements
+var div = document.querySelector('#content');
+var search = document.querySelector('#titlesearch');
+var button = document.querySelector('#searchBtn');
 
+//submit button gets it started
+searchBtn.addEventListener('click', function(ev) {
+  ev.preventDefault();
+  console.log(search.value);
+});
 
+var drop = document.querySelector('#dropbox');
+console.log(drop);
+var searchValue = search.value;
+var chosenEndPoint = drop.value;
+console.log(chosenEndPoint);
+var queryStr = '';
+if (chosenEndPoint === 'booktitle') {
+  queryStr = '?name=';
+} else if (chosenEndPoint === 'author') {
+  queryStr = '?title=';
+}
+queryStr += searchValue;
+console.log(queryStr);
+var endPoint = 'https://www.googleapis.com/books/v1/volumes?q=search+terms' + chosenEndPoint;
+var googleApiKey = '&apikey=' + GOOGLE_BOOKS_API_KEY;
+var url = endPoint + queryStr + googleApiKey;
+console.log(url);
 
   //ajax stuff
     $.ajax({
       url: url,
       dataType: 'json'
     }).done(function(response){
-      console.log("response: ", response);  
-};
+      console.log("response: ", response);
+    }
+
+)};
