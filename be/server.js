d4c96779de5a54073f669801789964d667583899
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2016-07-24 22:05:51 -0400

    i have worked hard on this. still can't get my favbtn to add data to db.

--- be/server.js
+++ be/server.js
@@ -1,8 +1,10 @@
+var PORT = 3000;
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var app = express();
+var request = require('request');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -10,10 +12,133 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // connect to the database server
 var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://localhost:27017/books';
-mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, database) {
+var url = 'mongodb://localhost:27017/books_db';
+
+mongodb.MongoClient.connect(process.env.GOOGLE_BOOKS_API_KEY || url, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
-}
+});
+
+//get books
+app.get("/books", function(req, res){
+  MongoClient.connect(mongoUrl, function (err, db) {
+    var favbooksCollection = db.collection('books');
+    if (err) {
+      // console.log('Unable to connect to the mongoDB server. ERROR:', err);
+    } else {
+      //get all
+      favbooksCollection.find().toArray(function (err, result) {
+     if (err) {
+       console.log("ERROR!", err);
+       response.json("error");
+     } else if (result.length) {
+       console.log('Found:', result);
+       response.json(result);
+     } else { //
+       console.log('No document(s) found with defined "find" criteria');
+       response.json("no favorite books found");
+     }
+     db.close(function() {
+       console.log( "database CLOSED");
+     });
+   }); // end find
+
+
+  // res.json({"description":"HI TIFFANY"});
+// });//end get
+
+app.get("/books/search", function(req, res){
+  res.json({"description":"taco"});
+});//end get
+
+
+// add to favorites
+app.post("/books/favorites", function(req, res){
+
+  // response.json({"description":"add new"});
+console.log("request.body", request.body);
+
+MongoClient.connect(mongoUrl, function (err, db) {
+  var boooksCollection = db.collection('books');
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. ERROR:', err);
+  } else {
+    // We are connected!
+    console.log('Connection established to', mongoUrl);
+    console.log('Adding new user...');
+
+  // res.send("queryId" + req.query.id);
+  // console.log('queryId' + JSON.stringify(req.query));
+
+  } // end else
+}); // end mongo connect
+}); // end add new
+
+
+/* delete */
+app.delete('/books/:title', function(request, response) {
+  // response.json({"description":"delete by name"});
+
+  console.log("request.body:", request.body);
+  console.log("request.params:", request.params);
+
+  MongoClient.connect(mongoUrl, function (err, db) {
+    var favbooksCollection = db.collection('books');
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. ERROR:', err);
+    } else {
+      // We are connected!
+      console.log('Deleting by title... ');
+
+      /* Delete */
+      favbooksCollection.remove(request.params, function(err, numOfRemovedDocs) {
+        console.log("numOfRemovedDocs:", numOfRemovedDocs);
+        if(err) {
+          console.log("error!", err);
+        } else { // after deletion, retrieve list of all
+          favbooksCollection.find().toArray(function (err, result) {
+            if (err) {
+              console.log("ERROR!", err);
+              response.json("error");
+            } else if (result.length) {
+              console.log('Found:', result);
+              response.json(result);
+            } else { //
+              console.log('No document(s) found with defined "find" criteria');
+              response.json("none found");
+            }
+            db.close(function() {
+              console.log( "database CLOSED");
+            });
+          }); // end find
+
+        } // end else
+      }); // end remove
+
+    } // end else
+  }); // end mongo connect
+
+}); // end delete
+
+
+app.post("/books", function(req, res){
+
+// stuff will go here
+
+var searchValue = req.body.searchValue;
+var chosenEndPoint = req.body.chosenEndPoint;
+
+request({
+  url: "https://www.googleapis.com/books/v1/volumes?q=search+terms" + searchValue,
+  method: "get",
+  callback: function(error, response, body){
+res.send(body);
+        }
+    });
+});//end post
+
+app.listen(PORT, function(){
+  console.log('listening on 3000...');
+});
--- fe/app.js
+++ fe/app.js
@@ -9,11 +9,9 @@ var button = document.querySelector('#searchBtn');
 //submit button gets it started
 searchBtn.addEventListener('click', function(ev) {
   ev.preventDefault();
-  console.log(search.value);
-});
 
 var drop = document.querySelector('#dropbox');
-console.log(drop);
+// console.log(drop);
 var searchValue = search.value;
 var chosenEndPoint = drop.value;
 console.log(chosenEndPoint);
@@ -25,17 +23,72 @@ if (chosenEndPoint === 'booktitle') {
 }
 queryStr += searchValue;
 console.log(queryStr);
-var endPoint = 'https://www.googleapis.com/books/v1/volumes?q=search+terms' + chosenEndPoint;
-var googleApiKey = '&apikey=' + GOOGLE_BOOKS_API_KEY;
-var url = endPoint + queryStr + googleApiKey;
-console.log(url);
 
+
+var data = {
+  searchValue: searchValue,
+  chosenEndPoint: chosenEndPoint
+    // whatever: whatever the user types into the box, eg. title, author
+};
   //ajax stuff
     $.ajax({
-      url: url,
+      url: "http://localhost:3000/books",
+      method: "post",
+      data: data,
       dataType: 'json'
-    }).done(function(response){
-      console.log("response: ", response);
+    }).done(function(res){
+      console.log("response: ", res);
+      appending(res);
+    });
+// let's see this mess, append to the DOM
+function appending(response) {
+  var html = '<ul>';
+    for (var i = 0; i < response.items.length; i++) {
+      var book = response.items[i];
+      var id = book.id;
+      var title = book.volumeInfo.title;
+      var thumbnail = book.volumeInfo.imageLinks.smallThumbnail;
+      var authors = [];
+      if (book.volumeInfo.hasOwnProperty('authors')) {
+         authors = book.volumeInfo.authors;
+      }
+      var description = book.searchInfo.textSnippet;
+      var price = 'no sale price available';
+      if (book.saleInfo.hasOwnProperty('retailPrice')) {
+          price = '$' + book.saleInfo.retailPrice.amount;
+      }
+      html += '<img src="'+thumbnail+'">';
+      html += '<h3>' + title + '</h3>';
+      html += '<p>Authors: ' ;
+      for (var j = 0; j < authors.length; j++) {
+        html += authors[j];
+        if (j !== authors.length - 1) {
+          html += ', ';
+        }
+      }
+      html += '</p>';
+      html += '<p>' + price + '</p>';
+      html += '<p>' + description + '</p>';
+      html += "<button id='" + id +"'  type='button' name='button'>Add to Favorites</button>"; // thank you Babajide for helping me with this button part.
     }
-
-)};
+  html += '</ul>';
+  document.querySelector('#content').innerHTML = html;
+  // here I want to create a favorite button using JS. I need a favorite button for every ID to appear. So I probably need to use a loop method.
+  var buttons = document.querySelectorAll('button');
+    for (var f = 0; f < buttons.length; f++) {
+      buttons[f].addEventListener('click', function(){
+        var id = this.id;
+        console.log('favorite button clicked: ', id);
+        $.ajax({
+          url: "http://localhost:3000/books/favorites",
+          method: "post",
+          data: {"id": id},
+          dataType: 'json'
+        }).done(function(res){
+          console.log("favorites response: ", res);
+        });
+      });
+    }
+} // end function
+});
+};
